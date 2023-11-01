e09b59d517d25790df81cdc7df65c01329fd0e63
Andrei Debu <100160043+andreixdebu@users.noreply.github.com>
2023-11-01 21:41:42 +0200

    Added navigation bar with buttons, logo, name. Added favicon, modified the site title. Added account button.

---
+++ proiectcolectiv2023/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import favicon from '../favicon.png';
+import account from '../account.png';
+import styles from '../styles/Navbar.module.css';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+
+        <div className={styles.leftContent}>
+            <img src={favicon} alt="Favicon" className={styles.navbarLogo} style={{ width: '32px', height: '32px' }} />
+            <h1 className={styles.navbarTitle}>FoodBytes</h1>
+        </div>
+
+        <div className={styles.navbarLinks}>
+
+          <div className={styles.navbarButtons}>
+              <a href="/">Home</a>
+          </div>
+
+          <div className={styles.navbarButtons}>
+              <a href="/restaurants">Our restaurants</a>
+          </div>
+
+          <div className={styles.navbarButtons}>
+              <a href="/cart">My cart</a>
+          </div>
+
+        </div>
+        
+        <div className={styles.navbarAccount} >
+          <img src={account} alt ="Account" style={{ width: '32px', height: '32px' }}/>
+        </div>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
