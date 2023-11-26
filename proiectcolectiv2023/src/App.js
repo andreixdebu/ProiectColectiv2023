c35445d660bed391a09140e561e6417de55e9bd7
sergiu <sergiu.dobocan@gmail.com>
2023-11-26 15:12:14 +0200

    added change password and login page to react router in App.js

--- proiectcolectiv2023/src/App.js
+++ proiectcolectiv2023/src/App.js
@@ -2,6 +2,8 @@ import Homepage from './components/Homepage/Homepage';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import RestaurantList from "./components/RestaurantList/RestaurantList";
+import ChangePasswordPage from "./components/ChangePasswordPage/ChangePasswordPage";
+import LoginPage from "./components/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/restaurant-list" element={<RestaurantList />} />
+        <Route path="/change-password" element={<ChangePasswordPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+
+
       </Routes>
     </Router>
   );
